137d04ae23f6586d6abdace91962bc50398518eb
Ali Can Gündüz <alicangunduzcom@gmail.com>
2023-06-30 05:05:42 +0300

    Dernek Yönetim Versiyon 1.0.01
    Fix : Üye ekleme kısmında id require kaldırıldı. Test kısmında otomatik form doldurma kullanıldığından style display olan id require doldurduğu fark edilmedi bu soruna çözüm getirdim.

--- uye-ekle.php
+++ uye-ekle.php
@@ -30,7 +30,7 @@
                                     <div class="relative z-0 mb-6 w-full group" style="display: none;">
                                         <input type="text" name="id" id="id"
                                             class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                                            placeholder=" " value="" required />
+                                            placeholder=" " value="" />
                                         <label for="id"
                                             class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
                                             Id</label>
